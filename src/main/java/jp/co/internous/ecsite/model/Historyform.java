fb0ae758af81be2075be3292032831cecf698c6d
taiki-yoshida971122 <taiki.yoshida971122@gmail.com>
2024-07-23 11:51:47 +0900

    Lesson_ECsite

---
+++ src/main/java/jp/co/internous/ecsite/model/Historyform.java
@@ -0,0 +1,15 @@
+package jp.co.internous.ecsite.model;
+
+import java.io.Serializable;
+
+public class Historyform implements Serializable {
+
+	private int userId;
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+}
